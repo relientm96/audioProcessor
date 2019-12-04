b4575d9340792f271d12762808dd1e240733e627
relientm96 <matthewyfy@gmail.com>
2019-12-04 22:20:55 +0800

    Added sine audio playing from library

--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,26 @@
 // Main Site Javascript
-$(document).ready(function() {
 
-    console.log("HELLLO");
+var playState = 0;
+
+var sineWave = new Pizzicato.Sound({
+    source: 'wave',
+    options: {
+        frequency: 900
+    }
 });
+
+$(document).ready(function() {
+    $("#playBtn").click(function() {
+        sineWave.stop();
+        sineWave.play();
+    });
+    $("#stopBtn").click(function() {
+        sineWave.stop();
+    });
+});
+
+// Get Path of Music file to play
+function getMusicFile() {
+
+
+}
